63c190c12aafe14ea712459567a843d79a865ca8
root <root@ip-172-31-43-208.ap-northeast-2.compute.internal>
2021-07-06 07:37:56 +0000

    add user,jwt,baserespone

---
+++ src/main/java/com/smileflower/santa/config/BaseResponseStatus.java
@@ -0,0 +1,82 @@
+package com.smileflower.santa.config;
+
+import lombok.Getter;
+
+/**
+ * 에러 코드 관리
+ */
+@Getter
+public enum BaseResponseStatus {
+    /**
+     * 1000 : 요청 성공
+     */
+    SUCCESS(true, 1000, "요청에 성공하였습니다."),
+
+
+    /**
+     * 2000 : Request 오류
+     */
+    // Common
+    REQUEST_ERROR(false, 2000, "입력값을 확인해주세요."),
+    EMPTY_JWT(false, 2001, "JWT를 입력해주세요."),
+    INVALID_JWT(false, 2002, "유효하지 않은 JWT입니다."),
+    INVALID_USER_JWT(false,2003,"권한이 없는 유저의 접근입니다."),
+
+    // users
+    USERS_EMPTY_USER_ID(false, 2010, "유저 아이디 값을 확인해주세요."),
+
+    // [POST] /users
+    POST_USERS_EMPTY(false,2014,"아직 입력하지 않은 필수항목이 있습니다."),
+    POST_USERS_EMPTY_EMAIL(false, 2015, "이메일을 입력해주세요."),
+    POST_USERS_INVALID_EMAIL(false, 2016, "이메일 형식을 확인해주세요."),
+    POST_USERS_EXISTS_EMAIL(false,2017,"중복된 이메일입니다."),
+    PASSWORD_CONFIRM_ERROR(false,2018,"두 비밀번호가 일치하지 않습니다."),
+    POST_USER_EXISTS_NAME(false,2019,"사용 중인 별명입니다."),
+    INSUFFICIENT_PW_RANGE(false,2020,"비밀번호를 8자 이상 입력해주세요."),
+    EXCEED_PW_RANGE(false,2021,"비밀번호를 16자 이하로 입력해주세요."),
+    INSUFFICIENT_NAME_RANGE(false,2022,"별명을 2~15자 내로 입력해주세요."),
+    USERS_INVALID_ACCESS(false,2023,"본인만 접근 가능합니다."),
+
+
+
+    /**
+     * 3000 : Response 오류
+     */
+    // Common
+    RESPONSE_ERROR(false, 3000, "값을 불러오는데 실패하였습니다."),
+
+    // [POST] /users
+    DUPLICATED_EMAIL(false, 3013, "중복된 이메일입니다."),
+    POST_USERS_NONEXIST_ACCOUNT(false,3014,"아이디 또는 비밀번호가 잘못되었습니다."),
+    ALREADY_LOGGED(false,3015,"이미 로그인되어 있습니다"),
+    ALREADY_LOGOUT(false,3016,"이미 로그아웃되어 있습니다."),
+
+
+
+    /**
+     * 4000 : Database, Server 오류
+     */
+    DATABASE_ERROR(false, 4000, "데이터베이스 연결에 실패하였습니다."),
+    SERVER_ERROR(false, 4001, "서버와의 연결에 실패하였습니다."),
+
+    //[PATCH] /users/{userIdx}
+    MODIFY_FAIL_USERNAME(false,4014,"유저네임 수정 실패"),
+
+    PASSWORD_ENCRYPTION_ERROR(false, 4011, "비밀번호 암호화에 실패하였습니다."),
+    PASSWORD_DECRYPTION_ERROR(false, 4012, "비밀번호 복호화에 실패하였습니다.");
+
+
+    // 5000 : 필요시 만들어서 쓰세요
+    // 6000 : 필요시 만들어서 쓰세요
+
+
+    private final boolean isSuccess;
+    private final int code;
+    private final String message;
+
+    private BaseResponseStatus(boolean isSuccess, int code, String message) {
+        this.isSuccess = isSuccess;
+        this.code = code;
+        this.message = message;
+    }
+}
--- src/test/java/com/smileflower/santa/SantaApplicationTests.java
+++ src/test/java/com/smileflower/santa/SantaApplicationTests.java
@@ -3,11 +3,11 @@ package com.smileflower.santa;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class SantaApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//@SpringBootTest
+//class SantaApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
