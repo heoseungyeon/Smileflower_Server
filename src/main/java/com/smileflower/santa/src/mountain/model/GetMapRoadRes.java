cf9fbbc2cc67142dc5a32e6868b8cba0982a09c6
heoseungyeon <dia0312@naver.com>
2021-07-21 10:21:57 +0900

    fix

--- src/main/java/com/smileflower/santa/src/mountain/model/GetMapRoadRes.java
+++ src/main/java/com/smileflower/santa/src/mountain/model/GetMapRoadRes.java
@@ -1,4 +1,19 @@
 package com.smileflower.santa.src.mountain.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class GetMapRoadRes {
+    private List<GetRoadRes> road;
+    private GetMapRes mountain;
+
+    public GetMapRoadRes() {
+
+    }
 }
