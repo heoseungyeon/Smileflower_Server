f5bd01dbaef9cf7723f15c9837e5ff8b6063182e
root <root@ip-172-31-1-237.ap-northeast-2.compute.internal>
2021-07-21 04:01:07 +0000

    fix getMapRoadRes

--- src/main/java/com/smileflower/santa/src/mountain/model/GetMapRoadRes.java
+++ src/main/java/com/smileflower/santa/src/mountain/model/GetMapRoadRes.java
@@ -4,16 +4,15 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter
 @Setter
-@AllArgsConstructor
+
 public class GetMapRoadRes {
-    private List<GetRoadRes> road;
     private GetMapRes mountain;
-
-    public GetMapRoadRes() {
-
+    private List<GetRoadRes> road = new ArrayList<>();
+    public GetMapRoadRes(){
     }
 }
